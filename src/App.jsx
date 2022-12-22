b49ab0f608422602eccecdf2b4f66807497d8336
rodrigovera424 <rodrigovera424@gmail.com>
2022-12-22 15:41:15 -0300

    en construccion

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Header from "./components/header/Header"
+import Nav from "./components/nav/Nav"
+import About from "./components/about/About"
+import Experience from "./components/Experience/Experience"
+import Services from "./components/Services/Services"
+import Portfolio from "./components/Portfolio/Portfolio"
+import Testimonios from './components/testimonios/Testimonios'
+import Contact from "./components/contact/Contact"
+import Footer from './components/Footer/Footer'
+const app = () => {
+  return (
+     <>
+         <Header/>
+         <Nav/>
+         <About/>
+         <Experience/>
+         <Services/>
+         <Portfolio/>
+         <Testimonios/>
+         <Contact/>
+         <Footer/>
+         </>
+
+
+    
+
+     )
+}
+
+export default app
